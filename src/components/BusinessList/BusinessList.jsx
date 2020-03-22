dca0c6fba572dad3801652837c4a8978bf6d768d
Faviola Caballero <fabycg18@gmail.com>
2020-03-22 15:53:45 -0700

    moved business object to app file and passed info from parent components to child components.

--- src/components/BusinessList/BusinessList.jsx
+++ src/components/BusinessList/BusinessList.jsx
@@ -6,14 +6,26 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
+    {/*  we manually did this in the BusinessList component so that we can
+    simulate a list of returned business.This will be refactored in order to
+    remove repetition.*/}
+     {/*<Business />
         <Business />
         <Business />
         <Business />
         <Business />
-        <Business />
-        <Business />
-     </div>
-    )
+        <Business />*/}
+    {/*Here we removed the repetition and accessed the businesses prop and
+    called the map method on it.Then we passed a callback function with one
+    parameter of business that returns a business component with a property
+    called business equal to the parameter of the callback function.*/}
+        {
+          this.props.businesses.map((business) => {
+          return <Business business={business} />
+          })
+        }
+      </div>
+    );
   }
 }
 
