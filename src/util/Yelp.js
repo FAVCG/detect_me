d857b674681533eb09a6522bc9c92c7f2ce97763
Faviola Caballero <fabycg18@gmail.com>
2020-03-26 17:31:08 -0700

    added functionality by quering yelp

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = "r7_fdvbU988DAkhBOxO0otxWiy77480oGWg_QxktQOSQdtUvNUBHH54YaShPJZKAYNZufqYh0gKCq_IQMBYFCi2s08B-Zu3KgqxVYmKf6h2R75FgmJvvNpSttSl9XnYx";
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            }
+          });
+        }
+    });
+  }
+};
+
+export default Yelp;
+
